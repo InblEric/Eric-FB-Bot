b95335fbfb2036bffecbcba7f12f957aa6c0f3f1
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2015-10-29 22:11:20 -0500

    echo bot

--- web.js
+++ web.js
@@ -1,1 +1,10 @@
-console.log('Server running at http://127.0.0.1:8124/');
+var login = require("facebook-chat-api");
+ 
+// Create simple echo bot 
+login({email: "FB_EMAIL", password: "FB_PASSWORD"}, function callback (err, api) {
+    if(err) return console.error(err);
+ 
+    api.listen(function callback(err, message) {
+        api.sendMessage(message.body, message.threadID);
+    });
+});
