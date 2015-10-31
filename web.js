6f5e57c63d8e1c97b9e84d15d611505146c864bb
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2015-10-31 14:31:35 -0500

    Add nfl games

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var login = require("facebook-chat-api");
+var nflScores = require("nfl_scores");
  
 var items=[
 "Hello from Matt Facts! Fact - Matt likes to work out a lot. Send 'STOP' to stop receiving these messages.",
@@ -13,13 +14,25 @@ login({email: process.env.EM, password: process.env.FP}, function callback (err,
  
     api.listen(function callback(err, message) {
         // api.sendMessage(message.body, message.threadID);
-        if(message.body === 'Matt Fact') {
-              var item = items[Math.floor(Math.random()*items.length)];
-              api.sendMessage(item, message.threadID);
-
+        if(message.type == "body") {
+	        if(message.body === 'Matt Fact') {
+        	      var item = items[Math.floor(Math.random()*items.length)];
+    	          api.sendMessage(item, message.threadID);
+	
+    	    }
+	        if(message.body === 'STOP') {
+            	api.sendMessage("'STOP' is an unrecognized command. Send 'STOP' to stop receiving these messages.", message.threadID);
+        	}
+        
+    	    if(message.body === 'NFL') {
+				nflScores.refresh(function(err, scores) {
+				    for (i = 0; i < scores.gms.length; i++) { 
+						console.log(scores.gms[i].vnn + " at " + scores.gms[i].hnn);
+					}
+				});
+	        }
         }
-        if(message.body === 'STOP') {
-            api.sendMessage("'STOP' is an unrecognized command. Send 'STOP' to stop receiving these messages.", message.threadID);
-        }
+        
+        
     });
 });
