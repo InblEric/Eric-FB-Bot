987125b77c15fbabab2f8d63b43b2aa45ac165c4
Eric Wehrmeister <eric.wehrmeister@utexas.edu>
2016-03-17 11:12:32 -0500

    Postgres

--- web.js
+++ web.js
@@ -4,6 +4,20 @@ var weather = require("weather-js")
 var weather_dict = {}
 var posts_dict = {}
 var posts = 0
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
  
 var items=[
 "Hello from Matt Facts! Fact - Matt likes to work out a lot. Send 'STOP' to stop receiving these messages.",
